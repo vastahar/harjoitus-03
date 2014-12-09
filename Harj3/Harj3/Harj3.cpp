cf89133d618537e5a8e45cf8eee48a7b53c8a5ce
vastahar <harri.vastamaki@gmail.com>
2014-12-09 06:20:32 +0200

    Harjoitus 3 ja 3a

---
+++ Harj3/Harj3/Harj3.cpp
@@ -0,0 +1,12 @@
+// HARJOITUS 3
+// Harri Vastamäki
+// IIOS14S1
+
+#include <iostream>
+using namespace std;
+
+void main()
+
+{
+	cout << "Hei Maailma!\n";
+}
---
+++ Harj3a/Harj3a/Harj3a.cpp
@@ -0,0 +1,22 @@
+// HARJOITUS 3a
+// Harri Vastamäki
+// IIOS14S1
+
+#include <iostream>
+using namespace std;
+
+void main()
+
+{
+	cout << "**********************************\n"
+		 << "**\n"
+		 << "* Harri *\n"
+		 << "* Vastamäki *\n"
+		 << "**\n"
+		 << "*-------------------------------------- *\n"
+		 << "**\n"
+		 << "* Keltinmäentie 21 A 8 *\n"
+		 << "* 40640 Jyväskylä *\n"
+		 << "**\n"
+		 << "**********************************\n";
+}
